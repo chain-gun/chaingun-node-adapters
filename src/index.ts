f79ed352d9f099be84b0b5feb2e5d0782dd503b7
go1dfish <me@go1dfish.me>
2019-10-27 00:01:05 +0000

    Initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export * from './lib/number';
+import { GunGraphAdapter } from '@chaingun/types';
+
+export default function createAdapter(): GunGraphAdapter {
+  const HTTP_URL = process.env.GUN_HTTP_URL || '';
+  const LMDB_PATH = process.env.GUN_LMDB_PATH || '';
+  const LMDB_MAP_SIZE =
+    parseInt(process.env.GUN_LMDB_MAP_SIZE || '', 0) || 1024 ** 3;
+
+  if (LMDB_PATH) {
+    return require('@chaingun/lmdb-adapter').createAdapter({
+      mapSize: LMDB_MAP_SIZE,
+      path: LMDB_PATH
+    });
+  } else if (HTTP_URL) {
+    return require('@chaingun/http-adapter').createAdapter(HTTP_URL);
+  }
+
+  throw new Error("Can't find Gun Adapter Config");
+}
